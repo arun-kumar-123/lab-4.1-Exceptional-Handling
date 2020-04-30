0361bdc3f6ec210ebf78fb4f35c867c4cac54c7f
arun-kumar-123 <63232493+arun-kumar-123@users.noreply.github.com>
2020-04-30 16:24:25 +0530

    arun kumar lab4.1 Exceptional Handling

---
+++ src/main/java/arun/cg/gla/spring/dao/ProductDaoInterface.java
@@ -0,0 +1,14 @@
+package arun.cg.gla.spring.dao;
+
+import java.util.List;
+
+import arun.cg.gla.spring.entity.Product;
+
+
+public interface ProductDaoInterface {
+	
+	public boolean create(Product product);
+	public List<Product> reterive();
+	
+
+}
---
+++ src/main/java/arun/cg/gla/spring/service/ProductServiceInterface.java
@@ -0,0 +1,14 @@
+package arun.cg.gla.spring.service;
+
+import java.util.List;
+
+import arun.cg.gla.spring.entity.Product;
+
+
+public interface ProductServiceInterface {
+	
+	public Product save(Product product);
+
+	public List<Product> reterive();
+
+}
